2a75ac229265bf6d8e172861acf172c4e679ce38
ddarahakit <ddarahakit@gmail.com>
2022-05-16 16:21:17 +0900

    테스트

---
+++ src/test/java/com/ddarahakit/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddarahakit.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
